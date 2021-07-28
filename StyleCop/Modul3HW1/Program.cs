c0208ed79747c65c844d9d815a15cd772bc65546
Dimasik2001 <dimonkalibaba@gmail.com>
2021-07-28 21:39:59 +0300

    empty project

---
+++ StyleCop/Modul3HW1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul3HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
